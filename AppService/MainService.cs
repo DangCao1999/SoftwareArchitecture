5d4cb9580f2debf3ae57a29f543a9bfe74fe7eb5
cao.nvd@itsstraining.edu.vn <cao.nvd@itsstraining.edu.vn>
2020-10-26 14:22:49 +0700

    Done

---
+++ AppService/MainService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+namespace AppService
+{
+    public partial class MainService : ServiceBase
+    {
+        public MainService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ChannelServices.RegisterChannel(new TcpChannel(6868), false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(SmartPhoneBUS), "abc", WellKnownObjectMode.SingleCall);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
